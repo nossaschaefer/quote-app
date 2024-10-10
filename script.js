b7e4b5b91e6cb614bf309c27a4ff8138009f3c91
Nossa Schaefer <nossa.schaefer@gmail.com>
2024-10-10 12:20:06 +0200

    chore: basic structure finished

---
+++ script.js
@@ -0,0 +1,24 @@
+const main = document.querySelector("main");
+const paragraphQuote = document.querySelector("#quote");
+const paragraphAuthor = document.querySelector("#author");
+const button = document.querySelector("button");
+
+function getQuote() {
+  fetch("https://dummy-apis.netlify.app/api/quote")
+    .then((response) => {
+      // console.log(response.status);
+      // console.log(response.url);
+      return response.json();
+    })
+    .then((data) => {
+      paragraphQuote.textContent = data.quote;
+      paragraphAuthor.textContent = "- " + data.author;
+    });
+}
+
+button.addEventListener("click", getQuote);
+
+window.onload = function () {
+  paragraphQuote.innerHTML = "1. Make it work <br> 2. Make it work better";
+  paragraphAuthor.textContent = "- Nico";
+};
