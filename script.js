5627db4546974e1cf5418055b009479fe163281b
Nossa Schaefer <nossa.schaefer@gmail.com>
2024-10-11 10:41:43 +0200

    chore: adds async function

--- script.js
+++ script.js
@@ -3,17 +3,29 @@ const paragraphQuote = document.querySelector("#quote");
 const paragraphAuthor = document.querySelector("#author");
 const button = document.querySelector("button");
 
-function getQuote() {
-  fetch("https://dummy-apis.netlify.app/api/quote")
-    .then((response) => {
-      // console.log(response.status);
-      // console.log(response.url);
-      return response.json();
-    })
-    .then((data) => {
-      paragraphQuote.textContent = data.quote;
-      paragraphAuthor.textContent = "- " + data.author;
-    });
+// function getQuote() {
+//   fetch("https://dummy-apis.netlify.app/api/quote")
+//     .then((response) => {
+//       // console.log(response.status);
+//       // console.log(response.url);
+//       return response.json();
+//     })
+//     .then((data) => {
+//       paragraphQuote.textContent = data.quote;
+//       paragraphAuthor.textContent = "- " + data.author;
+//     });
+// }
+
+// inzwischen üblicher als fetch
+async function getQuote() {
+  try {
+    const response = await fetch("https://dummy-apis.netlify.app/api/quote");
+    const data = await response.json();
+    paragraphQuote.textContent = data.quote;
+    paragraphAuthor.textContent = "- " + data.author;
+  } catch (err) {
+    console.log("error");
+  }
 }
 
 button.addEventListener("click", getQuote);
